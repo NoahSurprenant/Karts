912561927dcd3c9a4f294dd72ec9c25a9baf98ac
Noah Surprenant <noah.surprenant@gmail.com>
2019-09-10 19:23:46 -0400

    Blocking Movement Without Physics

--- Karts/Source/Karts/GoKart.cpp
+++ Karts/Source/Karts/GoKart.cpp
@@ -26,12 +26,24 @@ void AGoKart::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+
 	FVector Acceleration = Force / Mass;
 
 	Velocity = Velocity + Acceleration * DeltaTime;
 
+	UpdateLocationFromVelocity(DeltaTime);
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
 	FVector Translation = Velocity * 100 * DeltaTime;
-	AddActorWorldOffset(Translation);
+
+	FHitResult Hit;
+	AddActorWorldOffset(Translation, true, &Hit);
+	if (Hit.IsValidBlockingHit())
+	{
+		Velocity = FVector::ZeroVector;
+	}
 }
 
 // Called to bind functionality to input

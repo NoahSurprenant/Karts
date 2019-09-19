1d61cedf071bc1511a7c22cec8d9ff9a67875279
Noah Surprenant <noah.surprenant@gmail.com>
2019-09-19 17:19:53 -0400

    Steering And Turning Circles

--- Karts/Source/Karts/GoKart.h
+++ Karts/Source/Karts/GoKart.h
@@ -43,9 +43,9 @@ private:
 	UPROPERTY(EditAnywhere)
 	float MaxDrivingForce = 10000;
 
-	// The number of degrees rotated per second at full control throw (degress/s).
+	// The minimum radius of the car turning
 	UPROPERTY(EditAnywhere)
-	float MaxDegreesPerSecond = 90;
+	float MinTurningRadius = 10;
 
 	// Higher means more drag
 	UPROPERTY(EditAnywhere)

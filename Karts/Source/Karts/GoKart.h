983f0b98b64319e8b5cb2ab56b792a10e0ee76dd
Noah Surprenant <noah.surprenant@gmail.com>
2019-09-24 17:59:52 -0400

    Server Functions & Cheat Protection

--- Karts/Source/Karts/GoKart.h
+++ Karts/Source/Karts/GoKart.h
@@ -57,9 +57,11 @@ private:
 
 	FVector Velocity;
 
-	void MoveForward(float Value);
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveForward(float Value);
 
-	void MoveRight(float Value);
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveRight(float Value);
 
 	float Throttle;
 
